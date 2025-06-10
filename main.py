1f7350f1bad63ae1dde989406e80fb67c7779adf
iamwzl <wzl@wzl.to>
2025-06-10 13:23:05 +0100

    CH1:L6 - Verbose

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
+import sys
+import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 from exceptions import APIKeyError, NoMetadataError
 
@@ -12,16 +15,41 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
-    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--verbose', action='store_true', help="Enable verbose output")
+    parser.add_argument('prompt', help="Your prompt text")
+
+    args = parser.parse_args()
+
+    if not args.prompt:
+        parser.print_help()
+        sys.exit(1)
+
+    prompt = str.strip(args.prompt)
+    if prompt=="":
+        print("Your prompt can't just be whitespace.")
+        sys.exit(1)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=prompt,
+        contents=messages,
     )
-    if response.usage_metadata:
-        print("Response: ", response.text)
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count} | Response tokens: {response.usage_metadata.candidates_token_count}")
-    else:
+    if not response.usage_metadata:
         raise NoMetadataError("No usage_metadata returned with the client response, something went wrong.")
+
+    if args.verbose:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print("Response: ", response.text)
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        return
+
+    print(response.text)
+
 
 
 if __name__ == "__main__":
